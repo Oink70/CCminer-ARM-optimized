0fb41c7b91e6e4b7bf97235791b7f2362bdbb3f1
MetaStrawman <136662325+MetaStrawman@users.noreply.github.com>
2023-09-29 07:32:00 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,37 @@
-# To change the cuda arch, edit Makefile.am and run ./build.sh
+#!/bin/bash
 
-extracflags="-O3 -ffinite-loops -ffast-math -mfix-cortex-a53-835769 -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
+# Get CPU information
+arch=$(lscpu | awk '/Architecture:/ { print $2 }')
+model_name=$(lscpu | awk -F ': +' '/Model name:/ { print $2 }')
 
-./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8-a+crypto -mtune=cortex-a53" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=armv8-a+crypto -mtune=cortex-a53  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
+# Common compiler flags
+common_flags="-O3 -ffinite-loops -ffast-math -D_REENTRANT -finline-functions -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
 
+# Set architecture-specific flags
+if [[ "$arch" == "aarch64" ]]; then
+    if [[ "$model_name" == *"Cortex-A76"* ]]; then
+        cpu_flags="-mcpu=cortex-a76 -O3"
+    elif [[ "$model_name" == *"Cortex-A55"* ]]; then
+        cpu_flags="-mcpu=cortex-a55 -O3"
+    else
+        # Default to ARMv8-A architecture (Cortex-A53) if unknown
+        echo "Unknown or unsupported model: $model_name. Defaulting to ARMv8-A."
+        cpu_flags="-march=armv8-a+crypto -mtune=cortex-a53 -O3"
+    fi
+else
+    # Default to ARMv8-A architecture (Cortex-A53) if unknown architecture
+    echo "Unknown or unsupported architecture: $arch. Defaulting to Native Tuning."
+    cpu_flags="-march=armv8-a+crypto -mtune=cortex-a53 -O3"
+fi
+
+# Set vectorization flags
+vectorization_flags="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl"
+
+# Combine all flags
+all_flags="$common_flags $cpu_flags $vectorization_flags"
+
+# Configure and build
+./configure --target=aarch64-linux-gnu --host=x86_64-linux-gnu --build=x86_64-linux-gnu \
+            CXXFLAGS="-Wl,-hugetlbfs-align -funroll-loops -finline-functions $all_flags" \
+            CFLAGS="-Wl,-hugetlbfs-align -finline-functions $all_flags" \
+            CXX=clang++ CC=clang LDFLAGS="-v -flto -Wl,-hugetlbfs-align"
